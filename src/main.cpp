a9c7cb7131d4a3f9b481bdb7cc736b6483e9ba57
Enhex <enhex0@gmail.com>
2019-04-27 14:22:24 +0300

    added the ability to specify build directory via command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include "MSBuild.h"
 #include "Make.h"
 
+namespace fs = boost::filesystem;
+
 // return true if successfully found the build tool
-bool find_and_use_tool()
+bool find_and_use_tool(fs::path const& path)
 {
-	namespace fs = boost::filesystem;
-
-	auto const current_path = fs::current_path();
-
-	for (auto const& entry : fs::directory_iterator(current_path))
+	for (auto const& entry : fs::directory_iterator(path))
 	{
 		// check for Visual Studio solution file
 		if (entry.path().extension() == ".sln") {
@@ -24,10 +22,29 @@ bool find_and_use_tool()
 	return false;
 }
 
-int main()
+bool find_and_use_tool()
 {
-	if (!find_and_use_tool()) {
+	return find_and_use_tool(fs::current_path());
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc > 2) {
+		std::cerr << "Too many arguments.\n";
+		return EXIT_FAILURE;
+	}
+
+	bool success;
+
+	if (argc == 1) {
+		success = find_and_use_tool();
+	}
+	else {
+		success = find_and_use_tool(argv[1]);
+	}
+
+	if (!success) {
 		std::cerr << "could not find build tool file.\n";
-		return 1;
+		return EXIT_FAILURE;
 	}
 }
