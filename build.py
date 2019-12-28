25520eeffb8ae7faad1682556ff8f5c9a861cc7b
Enhex <enhex0@gmail.com>
2019-12-28 21:41:30 +0200

    add debug/release build scripts

--- build.py
+++ build.py
@@ -1,17 +1,39 @@
 import os
-import sys
+from sys import platform
 
-source = os.path.dirname(os.path.realpath(__file__))
-build_dir = '../build/'
+def create_symlink(src, dst):
+    # create all parent directories of the symlink one
+    parent_dir = os.path.dirname(dst)
+    os.makedirs(parent_dir, exist_ok=True)
+    
+    try:
+        os.symlink(src, dst)
+    except:
+        pass
 
-os.makedirs(build_dir, exist_ok=True)
-os.chdir(build_dir)
+def build(source, build_type, symlinks = [], symlink_pairs = []):
+    build_dir = '../build/' + build_type + '/'
 
-os.system('conan install "' + source + '/" --build=outdated')
+    # create build directory
+    os.makedirs(build_dir, exist_ok=True)
+    os.chdir(build_dir)
 
-os.chdir(source)
-if sys.platform.startswith('win'):
-    generator = 'vs2019'
-else:
-    generator = 'gmake2'
-os.system('premake5 ' + generator + ' --location="' + build_dir + '"')
+    # symlinks
+    for path in symlinks:
+        create_symlink(source + '/' + path, './' + path)
+
+    for src_path, dst_path in symlink_pairs:
+        create_symlink(source + '/' + src_path, './' + dst_path)
+
+    # conan
+    os.system('conan install "' + source + '/" --build=outdated -s arch=x86_64 -s build_type=' + build_type)
+
+    # choose generator based on OS
+    if platform == 'win32':
+        generator = 'vs2019'
+    else:
+        generator = 'gmake2'
+    
+    # premake
+    os.chdir(source)
+    os.system('premake5 ' + generator + ' --location="' + build_dir + '"')
